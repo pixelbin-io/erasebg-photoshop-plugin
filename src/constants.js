bf85783770dca46b38bdd10dadf1fee8fa81f5ac
Akash Panigrahi <akashpanigrahi@gofynd.com>
2024-01-16 23:18:38 +0530

    update package naming convention and make fields rendering dynamic

--- src/constants.js
+++ src/constants.js
@@ -1,14 +1,18 @@
 export const constants = {
     urls: {
-        redirectToAppsPage: "https://console.pixelbin.io/choose-org?redirectTo=settings/apps",
-        redirectToDashboardPage: "https://console.pixelbin.io/choose-org?redirectTo=dashboard",
-        redirectToPricingPage: "https://console.pixelbin.io/choose-org?redirectTo=settings/billing/pricing",
+        redirectToAppsPage:
+            "https://console.pixelbin.io/choose-org?redirectTo=settings/apps",
+        redirectToDashboardPage:
+            "https://console.pixelbin.io/choose-org?redirectTo=dashboard",
+        redirectToPricingPage:
+            "https://console.pixelbin.io/choose-org?redirectTo=settings/billing/pricing",
 
-        orgPricingPage: "https://console.pixelbin.io/organization/:orgId/settings/billing/pricing",
+        orgPricingPage:
+            "https://console.pixelbin.io/organization/:orgId/settings/billing/pricing",
 
         pluginHomePage: "https://www.erase.bg",
-        pluginDoc: "https://www.pixelbin.io/docs/tools/photoshop/erasebg",
+        pluginDoc: "https://www.pixelbin.io/docs/tools/photoshop/erasebg/",
 
         apiDomain: "https://api.pixelbin.io",
-    }
+    },
 };
